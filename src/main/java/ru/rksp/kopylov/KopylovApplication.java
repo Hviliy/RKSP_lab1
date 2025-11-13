264f45b91878140d5933a385368c0bbace08a7b9
Danila <danilakopylov603@gmail.com>
2025-11-13 15:59:45 +0800

    add

---
+++ src/main/java/ru/rksp/kopylov/KopylovApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.kopylov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopylovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopylovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/kopylov/KopylovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.kopylov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopylovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
